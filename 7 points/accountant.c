67dc30ee9fbad2b1e1bb0c287c7139f4a058897f
Kratosterra <44316137+Kratosterra@users.noreply.github.com>
2023-04-22 21:04:33 +0300

    Выполнено на 7 с клиент-серверной идеей выполнения

---
+++ 7 points/accountant.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <semaphore.h>
+
+#define SHM_NAME "/shared_memory"
+#define SEM_NAME "/shared_semaphore"
+
+typedef struct
+{
+    double sum;
+    int num_clients;
+    int client_id;
+} shared_data_t;
+
+shared_data_t *shared_area;
+
+double f(double x)
+{
+    return x * x / 1000.0;
+}
+
+double integrate(double a, double b, int all_op)
+{
+    double h = (b - a) / all_op;
+    double sum = 0.0;
+    double x;
+    int i;
+    for (i = 0; i < all_op; i++)
+    {
+        x = a + (i + 0.5) * h;
+        sum += f(x);
+    }
+    return h * sum;
+}
+
+void child_process(int i, double a, double b, int all_op, FILE *outfile)
+{
+    double area;
+    if (i > all_op)
+    {
+        return;
+    }
+    double step = (b - a) / (double)all_op;
+    area = integrate(a + (step * (double)(i - 1)), a + (step * (double)i), all_op);
+    shared_area->sum += area;
+    fprintf(outfile, "Счетовод [%d] считал %.2f - %.2f и получил: ", i, a + (step * (double)(i - 1)), a + (step * (double)i));
+    fprintf(outfile, "%lf кв.м\n", area);
+    return;
+}
+
+int main(int argc, char *argv[])
+{
+    double a, b;
+    FILE *infile, *outfile;
+    if (argc != 3)
+    {
+        fprintf(stderr, "Использование: %s <файл ввода> <файл вывода>\n", argv[0]);
+        exit(1);
+    }
+    if ((infile = fopen(argv[1], "r")) == NULL)
+    {
+        perror("Ошибка при открытии входного файла!\n");
+        exit(1);
+    }
+    if ((outfile = fopen(argv[2], "w")) == NULL)
+    {
+        perror("Ошибка при открытии выходного файла!\n");
+        exit(1);
+    }
+    if (fscanf(infile, "%lf %lf", &a, &b) != 2)
+    {
+        printf("Ошибка при чтении входных данных, убедитесь, что в файле ввода только 2 double числа.\n");
+        exit(1);
+    }
+    if (a < 0 || b < 0)
+    {
+        printf("Ошибка при чтении входных данных, убедитесь, что числа неотрицательные!\n");
+        exit(1);
+    }
+
+    // Открываем разделяемую память
+    int shm_fd = shm_open(SHM_NAME, O_RDWR, 0660);
+    if (shm_fd == -1)
+    {
+        perror("Ошибка при открытии разделяемой памяти");
+        exit(1);
+    }
+
+    // Отображаем разделяемую память в адресное пространство процесса
+    shared_area = mmap(NULL, sizeof(shared_data_t), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (shared_area == MAP_FAILED)
+    {
+        perror("Ошибка при отображении разделяемой памяти");
+        exit(1);
+    }
+
+    // Открываем семафор
+    sem_t *sem = sem_open(SEM_NAME, O_RDWR);
+    if (sem == SEM_FAILED)
+    {
+        perror("Ошибка при открытии семафора");
+        exit(1);
+    }
+
+    // Получаем текущее значение семафора
+    int sem_value;
+    sem_getvalue(sem, &sem_value);
+    int client_id = shared_area->num_clients + 1;
+
+    printf("Счетовод %d запущен. Текущее значение семафора: %d\n", client_id, sem_value);
+
+    // Ожидаем разрешения на доступ к разделяемой памяти
+    sem_wait(sem);
+
+    // Доступ к разделяемой памяти
+    child_process(client_id, a, b, shared_area->client_id, outfile);
+    printf("Счетовод %d: общая сумма = %f\n", client_id, shared_area->sum);
+    fprintf(outfile, "Счетовод %d: общая сумма = %f\n", client_id, shared_area->sum);
+    shared_area->num_clients += 1;
+
+    // Освобождаем разделяемую память
+    sem_post(sem);
+
+    // Закрываем разделяемую память
+    munmap(shared_area, sizeof(shared_data_t));
+    close(shm_fd);
+
+    // Закрываем семафор
+    sem_close(sem);
+    fclose(outfile);
+    fclose(infile);
+    printf("Счетовод %d завершен\n", client_id);
+    return 0;
+}
---
+++ 7 points/agronomist.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <semaphore.h>
+
+#define SHM_NAME "/shared_memory"
+#define SEM_NAME "/shared_semaphore"
+#define NUM_CLIENTS 5
+
+typedef struct
+{
+    double sum;
+    int count;
+    int num_clients;
+} shared_data_t;
+
+shared_data_t *shared_data;
+sem_t *semaphore;
+
+void cleanup()
+{
+    // Удаляем семафор и разделяемую память
+    sem_close(semaphore);
+    sem_unlink(SEM_NAME);
+    munmap(shared_data, sizeof(shared_data_t));
+    shm_unlink(SHM_NAME);
+}
+
+void sigint_handler(int signum)
+{
+    cleanup();
+    exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+    FILE *infile, *outfile;
+    if (argc != 4)
+    {
+        fprintf(stderr, "Использование: %s <файл ввода> <файл вывода> <кол-во независимых проццессов>\n", argv[0]);
+        exit(1);
+    }
+    if ((infile = fopen(argv[1], "r")) == NULL)
+    {
+        perror("Ошибка при открытии входного файла!\n");
+        exit(1);
+    }
+    int num_processes = atoi(argv[3]);
+    printf("Агроном приказал %d счетоводам разделится и наконец посчитать площадь!\n", num_processes);
+    if (num_processes < 1)
+    {
+        printf("Неправильное кол-во процессов: %s\n", argv[3]);
+        exit(1);
+    }
+    // Обработчик сигнала Ctrl+C
+    signal(SIGINT, sigint_handler);
+
+    // Создаем семафор
+    semaphore = sem_open(SEM_NAME, O_CREAT | O_EXCL, 0666, 1);
+    if (semaphore == SEM_FAILED)
+    {
+        perror("Ошибка при создании семафора");
+        exit(1);
+    }
+
+    // Создаем разделяемую память
+    int shm_fd = shm_open(SHM_NAME, O_CREAT | O_EXCL | O_RDWR, 0666);
+    if (shm_fd == -1)
+    {
+        perror("Ошибка при создании разделяемой памяти");
+        exit(1);
+    }
+
+    // Устанавливаем размер разделяемой памяти
+    if (ftruncate(shm_fd, sizeof(shared_data_t)) == -1)
+    {
+        perror("Ошибка при изменении размера разделяемой памяти");
+        exit(1);
+    }
+
+    // Отображаем разделяемую память в адресное пространство текущего процесса
+    shared_data = mmap(NULL, sizeof(shared_data_t), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (shared_data == MAP_FAILED)
+    {
+        perror("Ошибка при отображении разделяемой памяти");
+        exit(1);
+    }
+
+    // Инициализируем разделяемую память
+    shared_data->sum = 0;
+    shared_data->count = 0;
+    shared_data->num_clients = num_processes;
+
+    // Ожидаем завершения всех счетоводов.
+    while (true)
+    {
+        sem_wait(semaphore);
+        if (shared_data->count == num_processes)
+        {
+            break;
+        }
+        sem_post(semaphore);
+    }
+
+    if ((outfile = fopen(argv[2], "w")) == NULL)
+    {
+        perror("Ошибка при открытии выходного файла!\n");
+        exit(1);
+    }
+    printf("Завершаем..\n");
+    fprintf(outfile, "Агроном и счетоводы получили общую площадь: %.6f кв.м\n", shared_data->sum);
+    printf("Агроном и счетоводы получили общую площадь: %.6f кв.м\nПодробнее в файле вывода %s\n", shared_data->sum, argv[2]);
+    fclose(outfile);
+    fclose(infile);
+    cleanup();
+    return 0;
+}
